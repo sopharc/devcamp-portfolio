6f992614dccc290eb96fb853bc000b50ba01f7f8
Sophie Archer <hello@sophiearcher.co.uk>
2020-06-25 18:48:43 +0100

    create and new added

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,25 @@
 class PortfoliosController < ApplicationController
-def index
- @portfolio_items = Portfolio.all
-end
+  def index
+   @portfolio_items = Portfolio.all
+  end
+
+  def new
+   @portfolio_items = Portfolio.new
+  end
+
+  def create
+    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to @portfolio_items, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def show
+  end
 
 end
